14149d268bea0d03f53125db7820237d72849123
Nadeem <nadem.shahzad@gmail.com>
2016-11-10 12:01:51 +0500

    network_scan

---
+++ README.md
@@ -0,0 +1,10 @@
+Python script to scan network for live machines
+
+This is simple python script uses ping request to check a machine if it is alive and responding
+
+Usage:
+python network_scan.py
+
+Enter network id like 172.16.10.1
+Script will start the scan from IP 172.16.10.1 to 172.16.10.254, this will send simple ping packet to check if a machine
+respond to request and will print IP to screen if it is alive.
---
+++ network_scan.py
@@ -0,0 +1,33 @@
+import os
+import threading
+
+'''define network subnet '''
+
+def network_subnet(ip):
+    net=ip.split('.')
+    dot='.'
+    net=net[0]+dot+net[1]+dot+net[2]+dot
+    return net
+''' network scan to check machines
+    responding to ping request '''
+
+def network_scan(ip):
+        cmd="ping -c 1"+" "+ip
+        result=os.popen(cmd)
+        for p in result.readlines():
+            if p.count('ttl'):
+               print "[+] Node Alive--------->[%s]"%ip
+
+def start_scan(ip,st,en):
+        net=network_subnet(ip)
+        for index in xrange(st,en):
+            IP=net+str(index)
+            t=threading.Thread(target=network_scan,args=(IP,))
+            t.start()
+
+if __name__=='__main__':
+  IP=raw_input('Enter Network Address :')
+  try:
+     start_scan(IP,1,254)
+  except:
+     print "Give Network Id As: \n10.0.0.1"
